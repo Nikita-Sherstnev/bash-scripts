e98377455c3c99c5769c33bc69144b3d26b8f590
Nikita-Sherstnev <sherstnyov@vivaldi.net>
2020-09-28 19:04:13 +0400

    Added iteration in 4th script

--- 4.sh
+++ 4.sh
@@ -9,6 +9,17 @@ done
 echo "Low: $low";
 echo "High: $high";
 
-while read line; do
-	echo $line
-done
+readarray -t arr
+
+#declare -p arr
+
+let j=0;
+sum=0;
+for line in "${arr[@]}"
+    do
+        numbers_array[$j] = echo "$line" | grep -Eo '[+-]?[0-9]+([.][0-9]+)?';
+        $j+=1;    
+    done
+
+sum=$(IFS=+; echo "$((${numbers_array[*]}))")
+echo $sum
