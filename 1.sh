e29cdf5006da428b40009ce9d6fdc482e70d8c18
Nikita-Sherstnev <sherstnyov@vivaldi.net>
2020-10-04 22:31:52 +0400

    Pattern changed

--- 1.sh
+++ 1.sh
@@ -5,4 +5,4 @@ grep --include=$FILENAME_PATTERN -rli . -Ee $CONTENT_PATTERN
 # find -name $FILENAME_PATTERN -print0 | xargs -0 grep $CONTENT_PATTERN
 
 # FILENAME_PATTERN = '?g*r.txt'
-# CONTENT_PATTERN = 'con*tent'
+# CONTENT_PATTERN = 'con*tent?'
