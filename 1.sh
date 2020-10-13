d909394354f941c38f3af04fe6b9aba31e3a2944
Nikita-Sherstnev <sherstnyov@vivaldi.net>
2020-10-13 11:06:06 +0400

    1st script regex modified

--- 1.sh
+++ 1.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-grep --include=$FILENAME_PATTERN -rli . -Ee $CONTENT_PATTERN
+content="$(echo $CONTENT_PATTERN | sed 's/*/.*/' | sed 's/?/./')"
+grep --include=$FILENAME_PATTERN -rli . -Ee $content
 
 # grep -Eril $CONTENT_PATTERN . | xargs | grep -Ewio $FILENAME_PATTERN
 # find -name $FILENAME_PATTERN -print0 | xargs -0 grep $CONTENT_PATTERN
 
-# FILENAME_PATTERN = '?g*r.txt'
-# CONTENT_PATTERN = 'con*tent?'
+# export FILENAME_PATTERN='?g*r.txt'
+# export CONTENT_PATTERN='con*ten?'
